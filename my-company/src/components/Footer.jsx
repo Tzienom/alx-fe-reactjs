e17d3e3e251b8c37ab06d835b715e7ae3e732e38
Tzienom <tzienom@gmail.com>
2025-08-13 16:22:07 +0100

    Update: Add my-company website

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <footer
+      style={{
+        backgroundColor: "#333",
+        color: "white",
+        padding: "10px",
+        textAlign: "center",
+      }}
+    >
+      <p>&copy; 2025 My Company. All rights reserved.</p>
+    </footer>
+  );
+}
