8892abdfefc5a2ac82c57592dd454e5e2e760045
Tzienom <tzienom@gmail.com>
2025-08-13 12:23:32 +0100

    Update: rename ProfilePage.jsx to UserProfile.jsx

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,5 +1,5 @@
 import UserContext from "./components/UserContext";
-import ProfilePage from "./components/ProfilePage";
+import ProfilePage from "./components/UserProfile";
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
